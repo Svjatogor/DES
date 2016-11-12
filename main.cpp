5990ac2cb4c1f50491c0957f55c527ea6edd60a6
Timofey <vsjo.zanjato@gmail.com>
2016-11-12 18:50:40 +0300

    Added empty chars

--- main.cpp
+++ main.cpp
@@ -5,39 +5,22 @@
 
 using namespace std;
 
+void convert_bitset_to_str(string& str, const bitset<MAX_SIZE>& bit_str);
+
+void convert_str_to_bitset(bitset<MAX_SIZE>& bit_str, const string& str);
+
 int main() {
     string input_text = "fuck";
     const size_t str_size = sizeof(input_text);
-    bitset<str_size> input_bits;
+    bitset<MAX_SIZE> input_bits;
 
-    // conversion from string to bitset
-    for (int i = 0; i < input_text.length(); ++i) {
-        char c = input_text[i];
-        for (int j = 7; j >= 0 && c; --j) {
-            if (c & 0x1) {
-                input_bits.set(8 * i + j);
-            }
-            c >>= 1;
-        }
-    }
-
-    // conversion from bitset to string
-    string out_str = "";
-    for (int i = 0; i < str_size; i+=8) {
-        bitset<8> bit_char;
-        int q = 0;
-        for (int j = 7; j >=0; j--) {
-            bit_char[q] = input_bits[i + j];
-            q++;
-        }
-        out_str += char(bit_char.to_ulong());
-    }
-    cout << out_str;
+    // conversion data to bits array
+    convert_str_to_bitset(input_bits, input_text);
 
     /* DES algorithm */
     // add empty bits
     const size_t encrypt_size = 2 * str_size - 64 * ( str_size/64 );
-    bitset<encrypt_size> encrypt_bits;
+    bitset<MAX_SIZE> encrypt_bits;
     // copy from input data
     for (int i = 0; i < encrypt_size; i++) {
         if (i < str_size) {
@@ -46,7 +29,38 @@ int main() {
             encrypt_bits[i] = 0;
         }
     }
+    convert_bitset_to_str(input_text, encrypt_bits);
+    cout << input_text;
     // initial permutation
 
     return 0;
 }
+
+void convert_bitset_to_str(string& str, const bitset<MAX_SIZE>& bit_str) {
+    const size_t str_size = sizeof(bit_str);
+    // conversion from bitset to string
+    str = "";
+    for (int i = 0; i < str_size; i+=8) {
+        bitset<8> bit_char;
+        int q = 0;
+        for (int j = 7; j >=0; j--) {
+            bit_char[q] = bit_str[i + j];
+            q++;
+        }
+        str += char(bit_char.to_ulong());
+    }
+}
+
+void convert_str_to_bitset(bitset<MAX_SIZE>& bit_str, const string& str) {
+    // conversion from string to bitset
+    for (int i = 0; i < str.length(); ++i) {
+        char c = str[i];
+        for (int j = 7; j >= 0 && c; --j) {
+            if (c & 0x1) {
+                bit_str.set(8 * i + j);
+            }
+            c >>= 1;
+        }
+    }
+}
+
