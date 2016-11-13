332a363fa41f358802d82d59c25fdebc9a98b1b9
Timofey <vsjo.zanjato@gmail.com>
2016-11-13 19:30:47 +0300

    Added s_tranformation file and parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <fstream>
+#include <regex>
+#include <algorithm>
+#include <iterator>
+
 #define MAX_SIZE 40000
 
 using namespace std;
@@ -28,38 +33,7 @@ int expansion_indexes[48] =
         };
 
 // S transformation array
-14	4	13	1	2	15	11	8	3	10	6	12	5	9	0	7
-0	15	7	4	14	2	13	1	10	6	12	11	9	5	3	8
-4	1	14	8	13	6	2	11	15	12	9	7	3	10	5	0
-15	12	8	2	4	9	1	7	5	11	3	14	10	0	6	13
-15	1	8	14	6	11	3	4	9	7	2	13	12	0	5	10
-3	13	4	7	15	2	8	14	12	0	1	10	6	9	11	5
-0	14	7	11	10	4	13	1	5	8	12	6	9	3	2	15
-13	8	10	1	3	15	4	2	11	6	7	12	0	5	14	9
-10	0	9	14	6	3	15	5	1	13	12	7	11	4	2	8
-13	7	0	9	3	4	6	10	2	8	5	14	12	11	15	1
-13	6	4	9	8	15	3	0	11	1	2	12	5	10	14	7
-1	10	13	0	6	9	8	7	4	15	14	3	11	5	2	12
-7	13	14	3	0	6	9	10	1	2	8	5	11	12	4	15
-13	8	11	5	6	15	0	3	4	7	2	12	1	10	14	9
-10	6	9	0	12	11	7	13	15	1	3	14	5	2	8	4
-3	15	0	6	10	1	13	8	9	4	5	11	12	7	2	14
-2	12	4	1	7	10	11	6	8	5	3	15	13	0	14	9
-14	11	2	12	4	7	13	1	5	0	15	10	3	9	8	6
-4	2	1	11	10	13	7	8	15	9	12	5	6	3	0	14
-11	8	12	7	1	14	2	13	6	15	0	9	10	4	5	3
-12	1	10	15	9	2	6	8	0	13	3	4	14	7	5	11
-10	15	4	2	7	12	9	5	6	1	13	14	0	11	3	8
-9	14	15	5	2	8	12	3	7	0	4	10	1	13	11	6
-4	3	2	12	9	5	15	10	11	14	1	7	6	0	8	13
-4	11	2	14	15	0	8	13	3	12	9	7	5	10	6	1
-13	0	11	7	4	9	1	10	14	3	5	12	2	15	8	6
-1	4	11	13	12	3	7	14	10	15	6	8	0	5	9	2
-6	11	13	8	1	4	10	7	9	5	0	15	14	2	3	12
-13	2	8	4	6	15	11	1	10	9	3	14	5	0	12	7
-1	15	13	8	10	3	7	4	12	5	6	11	0	14	9	2
-7	11	4	1	9	12	14	2	0	6	10	13	15	3	5	8
-2	1	14	7	4	10	8	13	15	12	9	0	3	5	6	11
+int s_transformation_arr[8][3][15] = {};
 
 void convert_bitset_to_str(string& str, const bitset<MAX_SIZE>& bit_str);
 
@@ -67,11 +41,27 @@ void convert_str_to_bitset(bitset<MAX_SIZE>& bit_str, const string& str);
 
 bitset<32> feistel_function(const bitset<32>&right_part, const bitset<48> key);
 
+vector<string> split(const string& input, const string regex);
+
 int main() {
     string input_text = "fuck";
     const size_t str_size = sizeof(input_text);
     bitset<MAX_SIZE> input_bits;
-
+    // init s transformation array
+    ifstream infile("s_transformation");
+    if (!infile)
+        std::cerr << "Could not open the file!" << std::endl;
+    int i = 0;
+    int j = 0;
+    string line;
+    std::vector<string> b_arr;
+    string f, e;
+    while (getline(infile, line)) {
+        i++;
+        b_arr = split(line, "\t");
+        f = b_arr[0];
+        e = b_arr[b_arr.size() - 1];
+    }
     // conversion data to bits array
     convert_str_to_bitset(input_bits, input_text);
 
@@ -135,4 +125,14 @@ bitset<32> feistel_function(const bitset<32>&right_part, const bitset<48> key) {
     exapns_bits ^= key;
 
 }
+
+vector<std::string> split(const string& input, const string regex) {
+    // passing -1 as the submatch index parameter performs splitting
+    std::regex re(regex);
+    std::sregex_token_iterator
+            first{input.begin(), input.end(), re, -1},
+            last;
+    return {first, last};
+}
+
 
